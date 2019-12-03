3917981be3e09df457f1b3a81e86da92043ec8ac
Archit Sangal <55693745+architsangal@users.noreply.github.com>
2019-12-03 12:12:23 +0530

    Tower_Of_Hanoi

---
+++ Tower_Of_Hanoi.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void TowerOfHanoi(int n,char start,char mid,char end)
+{
+    if(n==1)
+    {
+        printf("Disk %d From %c To %c\n",1,start,end);
+        return;
+    }
+    TowerOfHanoi(n-1,start,end,mid);
+    printf("Disk %d From %c To %c\n",n,start,end);
+    TowerOfHanoi(n-1,mid,start,end);
+
+}
+int main(void)
+{
+    int n = 4;
+    TowerOfHanoi(n,'A','B','C');
+    return 0;
+}
